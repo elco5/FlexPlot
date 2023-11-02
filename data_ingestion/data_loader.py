123ed69ecd5441b283d267dda93b462a053dc426
ecountrywood <countrywoodster@gmail.com>
2023-11-02 02:10:51 -0700

    moved get file info into auto loader method

--- data_ingestion/data_loader.py
+++ data_ingestion/data_loader.py
@@ -1,41 +1,44 @@
 import os
+import sys
 
+sys.path.append('../')
 from utilities.logging import setup_logger
 
 logger = setup_logger()
+logger.info("Logging intitialized...")
 
-
-class DataLoader:
+class DataLoader: 
     def __init__(self, file_path: str) -> None:
         if not os.path.exists(file_path):
             logger.error(f"File not found: {file_path}")
             raise FileNotFoundError(f"File not found: {file_path}")
-
         self.file_info = {
             'full_path': None,
             'directory': None,
             'base_name': None,
             'type': None,
         }
-        self.metadata = {
-            'measurement_device': None,
-        }
         self.data = {
             'header': None,
             'dataframe': None,
         }
+        self.metadata = {
+            'measurement_device': None,
+        }
 
-        logger.info(f"DataLoader created with filename: {file_path}")
+        logger.info(f"DataLoader object succesfully created with filename: {file_path}")
+        
+        self.auto_load(file_path)
 
-    # def auto_load(self):
-    #     file_type = self.determine_file_type()
-    #     header_row, metadata = self.parse_metadata(file_type)
 
-    #     if header_row is None:
-    #         header_row = self.find_data_pattern(file_type)
-
-    #     df = self.load_data(file_type, header_row)
-    #     return df, metadata
+    ####################################################################
+    ### CLASS METHODS
+    ####################################################################
+    
+    def auto_load(self, file_path)-> None:
+        logger.info("Autoloader runining...")
+        self.get_file_attributes(file_path)
+    
 
     def get_file_attributes(self, file_path) -> None:
 
@@ -43,7 +46,7 @@ class DataLoader:
         # Extract the directory and base filename with extension
         self.file_info['directory'], self.file_info['base_name'] = os.path.split(file_path)
         self.file_info['type'] = os.path.splitext(self.file_info['base_name'])[1]
-
+        logger.info(f"File attributes succesfully populated")# with: {self.file_info}")
 
 
 
@@ -64,9 +67,17 @@ class DataLoader:
             # N is the number of lines you want to read
             lines = [f.readline().strip() for _ in range(lines_to_read)]
         
-        
-        
-        
+
+    # def auto_load(self):
+    #     file_type = self.determine_file_type()
+    #     header_row, metadata = self.parse_metadata(file_type)
+
+    #     if header_row is None:
+    #         header_row = self.find_data_pattern(file_type)
+
+    #     df = self.load_data(file_type, header_row)
+    #     return df, metadata
+            
         
         
         # # Read the specific lines that contain parts of the header based on equipment type
@@ -89,3 +100,7 @@ class DataLoader:
 
     #     final_header = date_time_parts + tag_comment_parts
     #     return final_header
+
+file_path = r"C:\Users\ecountrywood\dev\tools\FlexPlot\tests\sample_data\Overvoltage_264V_PM_0001.csv"
+
+data = DataLoader(file_path=file_path)
