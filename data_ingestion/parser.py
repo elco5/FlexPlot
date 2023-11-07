e9f2514510a3f1a75e55f0d04cd54028f8643f52
Eli Countrywood <countrywoodster@gmail.com>
2023-11-07 01:39:41 -0800

    working WT300 parser and header getter

---
+++ data_ingestion/parser.py
@@ -0,0 +1,51 @@
+
+
+# import sys
+# sys.path.append('../')
+# from utilities.logging import setup_logger
+
+# logger = setup_logger()
+
+
+metadata_dictionary = {
+    'WT3000': {
+        'keywords': ['Function,Expression,Unit',
+                     "F01:"],
+        'header_fetching_method': 'parser.fetch_wt3000_header',
+        'equipment_type': 'WT3000',
+        'sample_rate': 0.050 
+    },
+    'smartdaq': {
+        'keywords': ['SMARTDAC+ STANDARD', 'GM10'],
+        'header_fetching_method': 'self.fetch_smartdaq_header',
+    },
+
+}
+
+def fetch_wt3000_header(file_path):
+    # use the line that starts with 'Store No.,' as the heaer row   
+    header_row = None
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            if "Store No." in line:
+                print(line)
+                header_row = line
+                logger.info(f"parser found header row: {header_row}")
+                break
+    return header_row
+        
+def fetch_smartdaq_header(self):
+
+    # Find the lines that contain the parts of the header
+    tag_comment_line = [line for line in lines if "Tag Comment" in line][0]
+    date_time_line = [
+        line for line in lines if "Date" in line and "Time" in line][0]
+
+    # Extract the relevant parts and construct the final header
+    tag_comment_parts = tag_comment_line.split(
+        "\t")[2:]  # Adjust based on the actual structure
+    # Assuming "Date" and "Time" are the first two elements
+    date_time_parts = date_time_line.split("\t")[:2]
+
+    final_header = date_time_parts + tag_comment_parts
+    return final_header
