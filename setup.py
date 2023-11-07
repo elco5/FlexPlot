27bbed43f5720ff3068e19d701cc66eac64ce64f
Eli Countrywood <countrywoodster@gmail.com>
2023-11-07 01:59:46 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FlexPlot',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+        # 'numpy',
+        # 'pandas',
+        # etc.
+    ],
+    # Additional metadata about your package
+    author='Eli Countrywood',
+    author_email='countrywoodster@gmail.com',
+    description='Dash app to plot data from various sources',
+    # More information can be added as needed
+)
+#  pip install -e .
