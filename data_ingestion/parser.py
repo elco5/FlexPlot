ab86fa2fa998506ee435f254e1d673f140d9cb84
Eli Countrywood <countrywoodster@gmail.com>
2023-11-07 21:36:08 -0800

    working smartdaq header finding method

--- data_ingestion/parser.py
+++ data_ingestion/parser.py
@@ -1,10 +1,9 @@
 
 
-# import sys
-# sys.path.append('../')
-# from utilities.logging import setup_logger
 
-# logger = setup_logger()
+from utilities.logging import setup_logger
+
+logger = setup_logger()
 
 
 metadata_dictionary = {
@@ -13,7 +12,7 @@ metadata_dictionary = {
                      "F01:"],
         'header_fetching_method': 'parser.fetch_wt3000_header',
         'equipment_type': 'WT3000',
-        'sample_rate': 0.050 
+        # 'sample_rate': 0.050 
     },
     'smartdaq': {
         'keywords': ['SMARTDAC+ STANDARD', 'GM10'],
@@ -32,18 +31,28 @@ def fetch_wt3000_header(file_path):
                 break
     return header_row
         
-# def fetch_smartdaq_header(self):
+def fetch_smartdaq_header(file_path):
+    
+    lines = []
+    with open(file_path, "r", encoding="utf-8") as file:
+        lines = file.readlines()
 
-#     # Find the lines that contain the parts of the header
-#     tag_comment_line = [line for line in lines if "Tag Comment" in line][0]
-#     date_time_line = [
-#         line for line in lines if "Date" in line and "Time" in line][0]
+    # Find the lines that contain the parts of the header
+    tag_comment_line = [line for line in lines if "Tag Comment" in line][0]
+    date_time_line = [
+        line for line in lines if "Date" in line and "Time" in line][0]
 
-#     # Extract the relevant parts and construct the final header
-#     tag_comment_parts = tag_comment_line.split(
-#         "\t")[2:]  # Adjust based on the actual structure
-#     # Assuming "Date" and "Time" are the first two elements
-#     date_time_parts = date_time_line.split("\t")[:2]
+    # Extract the relevant parts and construct the final header
+    tag_comment_parts = tag_comment_line.split(
+        ",")[3:]  # Adjust based on the actual structure
+    # Assuming "Date" and "Time" are the first two elements
+    date_time_parts = date_time_line.split(",")[:3]
 
-#     final_header = date_time_parts + tag_comment_parts
-#     return final_header
+    final_header = date_time_parts + tag_comment_parts
+    # print(final_header)
+    return final_header
+
+if __name__ == "__main__":
+    file_path = r"../data/sample_data/smartdaq_sample.csv"
+    fetch_smartdaq_header(file_path)
+    logger.info("Done")
